f41379ae57c6371372476e21b18a4a9ba212a3f3
AlphaDVLPR <alphadvlpr@AlphaDVLPRs-MacBook-Pro.local>
2019-08-26 10:02:50 -0600

    Starting Point

---
+++ KarlTwitterClone/KarlTwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KarlTwitterClone
+//
+//  Created by AlphaDVLPR on 8/26/19.
+//  Copyright © 2019 AlphaDVLPR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
